d1f04a2aec0976ebaef9a52dfce871a8ce8bce68
Christian Simon <simon@swine.de>
2017-01-03 21:45:59 +0000

    Upgrade to redsocks release 0.5

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,28 @@
 FROM alpine:3.5
 
 RUN apk update && \
-    apk add redsocks bash iptables && \
+    apk add bash libevent && \
     rm -rf /var/cache/apk/*
+
+ENV DEV_PACKAGES alpine-sdk curl libevent-dev linux-headers
+
+ENV REDSOCKS_VERSION 0.5
+
+RUN apk update && \
+    apk add -s ${DEV_PACKAGES} > /tmp/temp-packages && \
+    apk add ${DEV_PACKAGES} && \
+    curl -sL -o /tmp/redsocks.tar.gz https://github.com/darkk/redsocks/archive/release-0.5.tar.gz && \
+    cd /tmp && \
+    tar xfz /tmp/redsocks.tar.gz && \
+    cd /tmp/redsocks-release-0.5 && \
+    make && \
+    mv redsocks /usr/bin && \
+    rm -rf /tmp/redsocks-release-0.5 && \
+    cat /tmp/temp-packages | head -n -1 | cut -d " " -f 3 | xargs apk del && \
+    rm -rf /tmp/redsocks.tar.gz /tmp/temp-packages && \
+    rm -rf /var/cache/apk/*
+
+RUN redsocks -v
 
 RUN addgroup -g 180 -S redsocks
 RUN adduser -u 180 -S redsocks redsocks
--- run.sh
+++ run.sh
@@ -1,32 +1,10 @@
 #!/usr/bin/env bash
-function append_redsocks_conf {
-  local type=$1
-  local ip=$2
-  local port=$3
-  local local_port=$4
-  if [ -z "$type" -o -z "$ip" -o -z "$port" -o -z "$local_port" ] ; then
-    echo missing required parameter >&2
-    exit 1
-  fi
-  (cat <<EOF
-redsocks {
-  type = $type;
-  ip = $ip;
-  port = $port;
-  local_ip = 127.0.0.1;
-  local_port = $local_port;
-  splice = false;
-}
-EOF
-) >> /tmp/redsocks.conf
-}
-
 function parse_ip {
-  echo $1 | sed -nE "s/^http(s)?:\/\/(.+):([0-9]+)$/\2/p"
+  echo "${1}" | sed -nE "s/^http(s)?:\/\/(.+):([0-9]+)$/\2/p"
 }
 
 function parse_port {
-  echo $1 | sed -nE "s/^http(s)?:\/\/(.+):([0-9]+)$/\3/p"
+  echo "${1}" | sed -nE "s/^http(s)?:\/\/(.+):([0-9]+)$/\3/p"
 }
 
 if [ $# -eq 1 ]; then
@@ -35,9 +13,19 @@ if [ $# -eq 1 ]; then
 fi
 
 if [ ! -z "$http_proxy" ] ; then
-  ip=$(parse_ip $http_proxy)
-  port=$(parse_port $http_proxy)
-  append_redsocks_conf "http-relay" $ip $port "12345"
+  ip=$(parse_ip "${http_proxy}")
+  port=$(parse_port "${http_proxy}")
+  (cat <<EOF
+redsocks {
+  type = http-relay;
+  ip = $ip;
+  port = $port;
+  local_ip = 127.0.0.1;
+  local_port = 12345;
+  listenq = 512;
+}
+EOF
+) >> /tmp/redsocks.conf
   echo
   echo "run the following command to redirect port 80 traffic the http proxy:"
   echo -e "\tiptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 12345"
@@ -45,9 +33,20 @@ if [ ! -z "$http_proxy" ] ; then
 fi
 
 if [ ! -z "$https_proxy" ] ; then
-  ip=$(parse_ip $https_proxy)
-  port=$(parse_port $https_proxy)
-  append_redsocks_conf "http-connect" $ip $port "12346"
+  ip=$(parse_ip "$https_proxy")
+  port=$(parse_port "$https_proxy")
+  (cat <<EOF
+redsocks {
+  type = http-connect;
+  ip = $ip;
+  port = $port;
+  local_ip = 127.0.0.1;
+  local_port = 12346;
+  listenq = 512;
+  on_proxy_fail = forward_http_err;
+}
+EOF
+) >> /tmp/redsocks.conf
   echo
   echo "run the following command to redirect port 443 traffic the https proxy:"
   echo -e "\tiptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 12346"
